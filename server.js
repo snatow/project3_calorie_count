c5af36c6038ab8efd8bb91a052f22ea88d61b9f6
Koty Wong <kotywong@Kotys-MacBook-Pro.local>
2016-06-17 10:26:50 -0400

    adding packages

--- server.js
+++ server.js
@@ -1,15 +1,32 @@
+// Dependencies
 var express = require('express');
 var app = express();
 var logger = require('morgan');
+var mongoose = require('mongoose')
+var MONGO_URI = process.env.MONGODB_URI || "mongodb://localhost/calorie_count_app_dev";
 var port = process.env.PORT || 3000
 
+// Middleware
 app.use(logger('dev'));
+app.use(express.static('public'));
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.use(methodOverride(function(req, res){
+  if (req.body && typeof req.body === 'object' && '_method' in req.body) {
+    var method = req.body._method;
+    delete req.body._method;
+    return method;
+  }
+}));
+app.use(cookieParser());
 
+mongoose.connect(MONGO_URI);
 
+// Controllers
 var testController = require('./controllers/test.js');
 app.use('/', testController);
 
-
+// Listener
 app.listen(port, function() {
   console.log('we are listening on ' + port)
 })
